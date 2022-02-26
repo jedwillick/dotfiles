e3a855dbc5fa42cbed82fbe9e49624a9a1aed412
jedwillick <jwillick@me.com>
2022-02-26 22:21:20 +1000

    Added install script for ubuntu

---
+++ ubuntu-install.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+sudo add-apt-repository -y ppa:git-core/ppa
+sudo apt update
+sudo apt upgrade -y
+sudo apt install -y git build-essential jq python3-pip zip unzip valgrind cowsay
+
+pip install --upgrade setuptools tqdm autopep8
+
+# Install Oh-My-Posh
+sudo wget -q --show-progress https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/posh-linux-amd64 -O /usr/local/bin/oh-my-posh
+sudo chmod +x /usr/local/bin/oh-my-posh
+
+# Oh-My-Posh Themes
+mkdir -p ~/.poshthemes
+wget -q --show-progress https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/themes.zip -O ~/.poshthemes/themes.zip
+unzip -oqq ~/.poshthemes/themes.zip -d ~/.poshthemes
+chmod u+rw ~/.poshthemes/*.json
+rm ~/.poshthemes/themes.zip
