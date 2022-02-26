c5308d1ec54c93e46151ad64dc321963e1994031
jedwillick <jwillick@me.com>
2022-02-26 14:20:37 +1000

    dotfiles

---
+++ setup.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+bak=/tmp/dotfiles.bak/$(date '+%d-%m-%Y_%H.%M.%S')
+mkdir -p $bak
+
+for file in dotfiles/.*[^.]; do
+    base=$(basename $file)
+    if [[ -d $file ]]; then
+        for sub in ${file}/*; do
+            link=~/${sub#dotfiles/}
+            mkdir -p $bak/$base && cp $link $_ 2>/dev/null | true
+            ln -sf $(pwd)/$sub $link
+        done
+    elif [[ -f $file ]]; then
+        cp ~/$base $bak/$base 2>/dev/null | true
+        ln -sf $(pwd)/$file ~/$base
+    fi
+done
+
+if [[ -z $WSL_DISTRO_NAME ]]; then
+    rm ~/.wsl_bashrc
+fi
