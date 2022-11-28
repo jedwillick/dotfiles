946068ceba207f38154b43e9ab5d74222da18ce6
jedwillick <jwillick@me.com>
2022-11-28 10:24:39 +1000

    feat: Add plugins, spotify-tui keybinds, and some options. plugins: tmux-resurrect, tmux-continuum, extrakto

--- dotfiles/.config/tmux/bootstrap.sh
+++ dotfiles/.config/tmux/bootstrap.sh
@@ -2,5 +2,5 @@
 
 if [[ ! -d "$TMUX_PLUGIN_MANAGER_PATH/tpm" ]]; then
   git clone https://github.com/tmux-plugins/tpm "$TMUX_PLUGIN_MANAGER_PATH/tpm"
-  tmux source "$TMUX_CONFIG_PATH/tmux.conf"
+  "$TMUX_PLUGIN_MANAGER_PATH"/tpm/bin/install_plugins
 fi
