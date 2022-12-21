267c6ac3c5965b0556d91b900148dc3ad43a159f
jedwillick <jwillick@me.com>
2022-12-21 15:21:27 +1000

    feat: add more maps

--- dotfiles/.config/nvim/lua/plugins/bufferline.lua
+++ dotfiles/.config/nvim/lua/plugins/bufferline.lua
@@ -19,18 +19,24 @@ local opts = { silent = true }
 
 -- These commands will navigate through bbuffers in order regardless of which mode you are using
 -- e.g. if you change the order of uffers in order regardless of which mode you are using
--- e.g. if you change the order of buffers :bnext and :bprevious will not respect the custom ordering
-set("n", "]b", ":BufferLineCycleNext<CR>", opts)
-set("n", "[b", ":BufferLineCyclePrev<CR>", opts)
+-- e.g. if you change the order of buffers <cmd>bnext and <cmd>bprevious will not respect the custom ordering
+set("n", "]b", "<cmd>BufferLineCycleNext<CR>", opts)
+set("n", "[b", "<cmd>BufferLineCyclePrev<CR>", opts)
 
 -- These commands will move the current buffer backwards or forwards in the bufferline
 set("n", "<leader>bn", ":BufferLineMoveNext<CR>", opts)
 set("n", "<leader>bp", ":BufferLineMovePrev<CR>", opts)
 
+-- Toggle the pin on current buffer
+set("n", "<leader>bp", "<cmd>BufferLineTogglePin<cr>", opts)
+
+-- Select a buffer
+set("n", "<leader>bb", "<cmd>BufferLinePick<cr>", opts)
+
 -- These commands will sort buffers by language or directory
-set("n", "<leader>bse", ":BufferLineSortByExtension<CR>", opts)
-set("n", "<leader>bsd", ":BufferLineSortByDirectory<CR>", opts)
+set("n", "<leader>bse", "<cmd>BufferLineSortByExtension<CR>", opts)
+set("n", "<leader>bsd", "<cmd>BufferLineSortByDirectory<CR>", opts)
 
 -- Delete buffers
-set("n", "<leader>bd", ":Bdelete<CR>", opts)
-set("n", "<leader>bD", ":Bdelete!<CR>", opts)
+set("n", "<leader>bd", "<cmd>Bdelete<CR>", opts)
+set("n", "<leader>bD", "<cmd>Bdelete!<CR>", opts)
