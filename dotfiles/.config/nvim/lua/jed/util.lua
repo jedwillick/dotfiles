122f82ae1927ad116051fbe26fcaac939498502c
jedwillick <jwillick@me.com>
2023-01-14 19:29:39 +1000

    fix(lsp): ignore copilot when attaching

--- dotfiles/.config/nvim/lua/jed/util.lua
+++ dotfiles/.config/nvim/lua/jed/util.lua
@@ -6,6 +6,9 @@ function M.on_attach(on_attach)
     callback = function(args)
       local buffer = args.buf
       local client = vim.lsp.get_client_by_id(args.data.client_id)
+      if client.name == "copilot" then
+        return -- Copilot doesn't need any extras
+      end
       on_attach(client, buffer)
     end,
   })
