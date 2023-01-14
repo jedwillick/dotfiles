fa284696268595e78e3ceea8c5cacd7b49cb1f37
jedwillick <jwillick@me.com>
2023-01-14 14:52:36 +1000

    feat: remove leap-ast.nvim

--- dotfiles/.config/nvim/lua/jed/plugins.lua
+++ dotfiles/.config/nvim/lua/jed/plugins.lua
@@ -161,15 +161,12 @@ return {
   {
     "ggandor/leap.nvim",
     event = "User VeryLazy",
-    dependencies = { "ggandor/flit.nvim", "ggandor/leap-ast.nvim" },
+    dependencies = { "ggandor/flit.nvim" },
     config = function()
       require("leap").add_default_mappings()
       require("flit").setup {
         labeled_modes = "nv",
       }
-      vim.keymap.set({ "n", "x", "o" }, "M", function()
-        require("leap-ast").leap()
-      end, {})
     end,
   },
   {
