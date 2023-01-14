294ff39d8c4e213a6def87b13cd7ece5e99a8fb2
jedwillick <jwillick@me.com>
2023-01-14 14:23:33 +1000

    fix: use LazyPlugin opts

--- dotfiles/.config/nvim/lua/jed/plugins/neo-tree.lua
+++ dotfiles/.config/nvim/lua/jed/plugins/neo-tree.lua
@@ -1,41 +1,31 @@
 return {
   "nvim-neo-tree/neo-tree.nvim",
-  branch = "v2.x",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "kyazdani42/nvim-web-devicons",
-    "MunifTanjim/nui.nvim",
+  cmd = "Neotree",
+  keys = {
+    { [[\]], "<cmd>Neotree<cr>" },
   },
-  lazy = false,
-  -- cmd = "Neotree",
-  -- keys = [[\]],
   init = function()
-    vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
+    vim.g.neo_tree_remove_legacy_commands = 1
   end,
-  config = function()
-    require("neo-tree").setup {
-      source_selector = {
-        winbar = true,
+  opts = {
+    source_selector = {
+      winbar = true,
+    },
+    window = {
+      mappings = {
+        ["<space>"] = false,
       },
-      window = {
-        mappings = {
-          ["<space>"] = false,
+    },
+    filesystem = {
+      follow_current_file = true,
+      use_libuv_file_watcher = true,
+      filtered_items = {
+        hide_dotfiles = false,
+        hide_by_name = {
+          ".git",
+          ".svn",
         },
       },
-      filesystem = {
-        follow_current_file = true,
-        use_libuv_file_watcher = true,
-        hijack_netrw_behavior = "open_default",
-        filtered_items = {
-          hide_dotfiles = false,
-          hide_by_name = {
-            ".git",
-            ".svn",
-          },
-        },
-      },
-    }
-
-    vim.keymap.set("n", [[\]], "<cmd>Neotree<cr>")
-  end,
+    },
+  },
 }
