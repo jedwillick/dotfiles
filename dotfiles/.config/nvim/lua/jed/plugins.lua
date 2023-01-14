bcd814562ac7a4071adf7d81f1b1d742ec440e61
jedwillick <jwillick@me.com>
2023-01-14 14:26:56 +1000

    feat: add LuaSnip and copilot to cmp module

--- dotfiles/.config/nvim/lua/jed/plugins.lua
+++ dotfiles/.config/nvim/lua/jed/plugins.lua
@@ -17,15 +17,6 @@ return {
     event = "InsertEnter",
   },
   {
-    "L3MON4D3/LuaSnip",
-    dependencies = {
-      "rafamadriz/friendly-snippets",
-      config = function()
-        require("luasnip.loaders.from_vscode").lazy_load()
-      end,
-    },
-  },
-  {
     "numToStr/Comment.nvim",
     config = function()
       require("Comment").setup()
